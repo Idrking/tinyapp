4b817670f477f09785ce13fe49b0ff2c7d9f1907
Isaac King <idrkings@gmail.com>
2021-01-20 01:23:50 +0000

    Changed header depending on status of login

--- express-server.js
+++ express-server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
 
 const PORT = 8080;
 
@@ -26,21 +30,19 @@ app.get('/u/:shortURL', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {username: req.cookies['username'], urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {username: req.cookies['username']};
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {username: req.cookies['username'], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
-
-
-
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
